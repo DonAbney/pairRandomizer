3858a3a0c875858fcca268e38702c5833f234f06
Don Abney <tdonaldabney@gmail.com>
2013-10-16 15:56:17 -0400

    The last of it

--- src/main/groovy/PairRandomizer.groovy
+++ src/main/groovy/PairRandomizer.groovy
@@ -1,6 +1,12 @@
 class PairRandomizer {
     
-    public readList(File developerNames) {
-        developerNames.readLines().size()
+    public readNameFile(File developerNames) {
+        developerNames.readLines()
+    }
+
+    public pairDevelopers(developerNames) {
+        Collections.shuffle(developerNames)
+        int pairSize  = 2
+        developerNames.collate(pairSize)
     }
 }
--- src/test/groovy/PairRandomizerTest.groovy
+++ src/test/groovy/PairRandomizerTest.groovy
@@ -9,13 +9,36 @@ class PairRandomizerTest extends GroovyTestCase {
     }
 
     void tearDown() {
-       developerNames.delete() 
+        developerNames.delete() 
     }
 
-    void testReadListCanCountNames() {
+    void testReadNameFileCanReturnListOfAllNames() {
         PairRandomizer pairRandomizer = new PairRandomizer()
         developerNames << 'Don\r\nDJ\r\nKyle'
-        assertEquals(3, pairRandomizer.readList(developerNames))
-    }   
+        def expectedNames = ['Don', 'DJ', 'Kyle']
+        def actualNames = []
+        assertEquals(expectedNames, pairRandomizer.readNameFile(developerNames)) 
+    }
 
+    void testReorderNamesReturnsFourNamesInTwoLists() {
+        PairRandomizer pairRandomizer = new PairRandomizer()
+        def developerNames = ['Don', 'DJ', 'Kyle', 'Angelo']
+        def actualOutput = pairRandomizer.pairDevelopers(developerNames)
+        assertEquals(2, actualOutput.size())
+    }
+
+    void testReorderNamesReturnsTenNamesInFiveLists() {
+        PairRandomizer pairRandomizer = new PairRandomizer()
+        def developerNames = ['Don', 'DJ', 'Kyle', 'Angelo','Bob','Earl','Jeff','Jon','Jason','Kristen']
+        def actualOutput = pairRandomizer.pairDevelopers(developerNames)
+        assertEquals(5, actualOutput.size())
+    }
+
+    void testPairDevelopersReturnsDifferentPairsForTwoInvocations() {
+        PairRandomizer pairRandomizer = new PairRandomizer()
+        def developerNames = ['Don', 'DJ', 'Kyle', 'Angelo','Bob','Earl','Jeff','Jon','Jason','Kristen']
+        def actualOutputOne = pairRandomizer.pairDevelopers(developerNames)
+        def actualOutputTwo = pairRandomizer.pairDevelopers(developerNames)
+        assert actualOutputOne != actualOutputTwo
+    }
 }
