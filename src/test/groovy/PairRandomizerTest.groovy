dff251c394f1d7c8b922472794186aecb99ff0ab
Don Abney <tdonaldabney@gmail.com>
2013-10-16 16:59:12 -0400

    DRY up tests

--- src/test/groovy/PairRandomizerTest.groovy
+++ src/test/groovy/PairRandomizerTest.groovy
@@ -2,22 +2,25 @@ import groovy.util.GroovyTestCase
 
 class PairRandomizerTest extends GroovyTestCase {
 
+    def pairRandomizer
+
+    void setUp() {
+        pairRandomizer = new PairRandomizer()
+    }
+
     void testPairDevelopersReturnsFourNamesInTwoLists() {
-        PairRandomizer pairRandomizer = new PairRandomizer()
         def developerNames = ['Don', 'DJ', 'Kyle', 'Angelo']
         def actualOutput = pairRandomizer.pairDevelopers(developerNames)
         assertEquals(2, actualOutput.size())
     }
 
     void testPairDevelopersReturnsTenNamesInFiveLists() {
-        PairRandomizer pairRandomizer = new PairRandomizer()
         def developerNames = ['Don', 'DJ', 'Kyle', 'Angelo','Bob','Earl','Jeff','Jon','Jason','Kristen']
         def actualOutput = pairRandomizer.pairDevelopers(developerNames)
         assertEquals(5, actualOutput.size())
     }
 
     void testPairDevelopersReturnsDifferentPairsForTwoInvocations() {
-        PairRandomizer pairRandomizer = new PairRandomizer()
         def developerNames = ['Don', 'DJ', 'Kyle', 'Angelo','Bob','Earl','Jeff','Jon','Jason','Kristen']
         def actualOutputOne = pairRandomizer.pairDevelopers(developerNames)
         def actualOutputTwo = pairRandomizer.pairDevelopers(developerNames)
