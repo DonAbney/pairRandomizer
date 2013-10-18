6eda4f4e13ed02bec73e79693dc7e0513c16d636
Don Abney <tdonaldabney@gmail.com>
2013-10-18 10:15:49 -0400

    Passing test to randomize the pairs

--- src/main/groovy/PairRandomizer.groovy
+++ src/main/groovy/PairRandomizer.groovy
@@ -5,6 +5,7 @@ class PairRandomizer {
     def returnPairs(list) {
         if(list == null) throw new EmptyListException()
         if(list.size() == 0) return list
+        Collections.shuffle(list)
         list.collate 2
     }
 }   
